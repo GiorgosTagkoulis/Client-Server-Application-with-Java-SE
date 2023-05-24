d929fe295de29827033bbde1365e6371ea3c688e
Georgios Tagkoulis <georgios.tagkoulis@netlight.com>
2023-05-24 16:22:05 +0200

    Add better user prompt for the client

--- client/src/main/java/com/georgios/StartUpClient.java
+++ client/src/main/java/com/georgios/StartUpClient.java
@@ -1,21 +1,69 @@
 package com.georgios;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import com.georgios.client.Client;
 
 public class StartUpClient {
-    private static final String host = "localhost";
-    private static final int port = 8000;
+    private String host;
+    private final int port = 8000;
+
+    void setHost() {
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+        try {
+            String temp;
+            while (true) {
+                System.out.println(
+                        "If you want to exit, type: exit()\nMake sure the server is running locally.\nDo you want to connect to localhost?\nType 'y':");
+                temp = reader.readLine();
+                if (temp.equals("exit()")) {
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                }
+                if (!temp.equals("y")) {
+                    System.out.println("Invalid input. Please try again.\n");
+                    continue;
+                }
+                if (temp.equals("y")) {
+                    host = "localhost";
+                    break;
+                } else if (temp.equals("no")) {
+                    continue;
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("StartUpClient:setPort:IOException:" + e.getMessage());
+            System.out.println("Defaulting to localhost");
+            host = "localhost";
+        }
+    }
+
+    String getHost() {
+        return this.host;
+    }
+
+    int getPort() {
+        return this.port;
+    }
 
     public static void main(String[] args) {
 
-        Client client = new Client(host, port);
+        StartUpClient startUpClient = new StartUpClient();
 
-        try {
-            client.start();
-        } catch (IOException e) {
-            System.out.println("StartUpClient:main:IOException:" + e.getMessage());
+        while (true) {
+            try {
+                startUpClient.setHost();
+                Client client = new Client(startUpClient.getHost(), startUpClient.getPort());
+                client.start();
+                System.out.println();
+            } catch (IOException e) {
+                System.out.println("StartUpClient:main:IOException:" + e.getMessage());
+            }
         }
+
     }
 }
